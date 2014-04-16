89bf4b1568aa6e11bd3210016016d62ef2fc9f87
oddhack <oddhack@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2014-04-16 12:35:38 +0000

    git-svn-id: https://cvs.khronos.org/svn/repos/ogl/trunk/doc/registry/public/api@26290 e7fa87d3-cd2b-0410-9028-fcbf551c1848

--- GL/glxext.h
+++ GL/glxext.h
@@ -36,7 +36,7 @@ extern "C" {
 ** Khronos $Revision$ on $Date$
 */
 
-#define GLX_GLXEXT_VERSION 20140317
+#define GLX_GLXEXT_VERSION 20140416
 
 /* Generated C header for:
  * API: glx
@@ -289,6 +289,23 @@ GLXContext glXImportContextEXT (Display *dpy, GLXContextID contextID);
 void glXFreeContextEXT (Display *dpy, GLXContext context);
 #endif
 #endif /* GLX_EXT_import_context */
+
+#ifndef GLX_EXT_stereo_tree
+#define GLX_EXT_stereo_tree 1
+typedef struct {
+    int type;
+    unsigned long serial;
+    Bool send_event;
+    Display *display;
+    int extension;
+    int evtype;
+    GLXDrawable window;
+    Bool stereo_tree;
+} GLXStereoNotifyEventEXT;
+#define GLX_STEREO_TREE_EXT               0x20F5
+#define GLX_STEREO_NOTIFY_MASK_EXT        0x00000001
+#define GLX_STEREO_NOTIFY_EXT             0x00000000
+#endif /* GLX_EXT_stereo_tree */
 
 #ifndef GLX_EXT_swap_control
 #define GLX_EXT_swap_control 1
--- GL/wglext.h
+++ GL/wglext.h
@@ -41,7 +41,7 @@ extern "C" {
 #include <windows.h>
 #endif
 
-#define WGL_WGLEXT_VERSION 20140317
+#define WGL_WGLEXT_VERSION 20140416
 
 /* Generated C header for:
  * API: wgl
