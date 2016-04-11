097cd218117f452ea5357c665ea7bb5c2b819d11
oddhack <oddhack@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2016-04-11 09:43:36 +0000

    git-svn-id: https://cvs.khronos.org/svn/repos/ogl/trunk/doc/registry/public/api@32614 e7fa87d3-cd2b-0410-9028-fcbf551c1848

--- GL/glext.h
+++ GL/glext.h
@@ -53,7 +53,7 @@ extern "C" {
 #define GLAPI extern
 #endif
 
-#define GL_GLEXT_VERSION 20160311
+#define GL_GLEXT_VERSION 20160411
 
 /* Generated C header for:
  * API: gl
--- GL/glxext.h
+++ GL/glxext.h
@@ -36,7 +36,7 @@ extern "C" {
 ** Khronos $Revision$ on $Date$
 */
 
-#define GLX_GLXEXT_VERSION 20160209
+#define GLX_GLXEXT_VERSION 20160411
 
 /* Generated C header for:
  * API: glx
@@ -316,6 +316,11 @@ GLXContext glXImportContextEXT (Display *dpy, GLXContextID contextID);
 void glXFreeContextEXT (Display *dpy, GLXContext context);
 #endif
 #endif /* GLX_EXT_import_context */
+
+#ifndef GLX_EXT_libglvnd
+#define GLX_EXT_libglvnd 1
+#define GLX_VENDOR_NAMES_EXT              0x20F6
+#endif /* GLX_EXT_libglvnd */
 
 #ifndef GLX_EXT_stereo_tree
 #define GLX_EXT_stereo_tree 1
